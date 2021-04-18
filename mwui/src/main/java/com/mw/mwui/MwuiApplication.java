f9367569b7ee332a49e20ff051db5b2e22a8a3c2
ssorry123 <syd03160@gmail.com>
2021-04-18 17:49:31 +0900

    init

---
+++ mwui/src/main/java/com/mw/mwui/MwuiApplication.java
@@ -0,0 +1,13 @@
+package com.mw.mwui;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MwuiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MwuiApplication.class, args);
+	}
+
+}
